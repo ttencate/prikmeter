593ca665e1205fbbe1822bfa560ae2554d61d55a
Thomas ten Cate <ttencate@gmail.com>
2017-09-30 20:15:38 +0200

    Connect to wifi

---
+++ smartmetertap/README.md
@@ -0,0 +1,4 @@
+You need to create a file named `"config.h` which looks like this:
+
+    #define WIFI_SSID "your_wifi_network_name"
+    #define WIFI_PASSWORD "your_wifi_password"
--- smartmetertap/smartmetertap.ino
+++ smartmetertap/smartmetertap.ino
@@ -1,5 +1,8 @@
 #include <Arduino.h>
+#include <ESP8266WiFi.h>
 #include <SoftwareSerial.h>
+
+#include "config.h"
 
 #define SERIAL_RX_PIN D5
 #define BUFFER_SIZE_BYTES 64
@@ -7,9 +10,21 @@
 SoftwareSerial p1(SERIAL_RX_PIN, -1, true, BUFFER_SIZE_BYTES);
 
 void setup() {
+  Serial.begin(115200);
+
+  Serial.println("Opening P1 port");
   p1.begin(115200);
 
-  Serial.begin(115200);
+  Serial.println("Connecting to wifi");
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+
   Serial.println("Up and running");
 }
 
