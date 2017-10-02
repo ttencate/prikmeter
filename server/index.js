807948f7f2b2250cf09b004fa7a99064a410c21b
Thomas ten Cate <ttencate@gmail.com>
2017-10-02 19:30:59 +0200

    Add simple sink server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const bodyParser = require('body-parser')
+const express = require('express')
+
+const app = express()
+
+// Populates req.body as a Buffer.
+app.use(bodyParser.raw({ type: 'text/plain' }))
+
+app.post('/telegrams', function (req, res) {
+  const telegram = req.body.toString()
+  console.log(telegram)
+
+  res.sendStatus(200)
+})
+
+app.listen(3000)
