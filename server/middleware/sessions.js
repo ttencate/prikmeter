348a7d3f471daadf6c0cbc92f0eb88104a019af1
Thomas ten Cate <ttencate@gmail.com>
2017-10-17 15:10:48 +0200

    Store sessions in the database

--- server/middleware/sessions.js
+++ server/middleware/sessions.js
@@ -1,6 +1,8 @@
 const crypto = require('crypto')
 const expressSession = require('express-session')
+const knexSessionStore = require('connect-session-knex')
 
+const db = require('../core/db')
 const settings = require('../services/settings')
 
 const COOKIE_SECRET_KEY = 'cookieSecret'
@@ -14,12 +16,22 @@ async function getCookieSecret () {
   return secret
 }
 
+function createStore() {
+  const KnexSessionStore = knexSessionStore(expressSession)
+  return new KnexSessionStore({
+    tablename: 'sessions',
+    knex: db,
+    createtable: true
+  })
+}
+
 module.exports.install = async function install (app) {
   const cookieSecret = await getCookieSecret()
   app.use(expressSession({
     cookie: { httpOnly: true },
     secret: cookieSecret,
     saveUninitialized: false,
+    store: createStore(),
     resave: false
   }))
 }
