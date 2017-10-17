267fb042085dfe0814e5611ee6380716bade78f8
Thomas ten Cate <ttencate@gmail.com>
2017-10-17 15:04:25 +0200

    Fix readings creation bug

--- server/services/readings.js
+++ server/services/readings.js
@@ -1,29 +1,42 @@
 const db = require('../core/db')
 
+const ELECTRICITY_KEYS = ['meterId', 'timestamp', 'totalConsumptionKwhLow', 'totalConsumptionKwhHigh', 'totalProductionKwhLow', 'totalProductionKwhHigh', 'currentConsumptionKw', 'currentProductionKw']
+const GAS_KEYS = ['meterId', 'timestamp', 'totalConsumptionM3']
+
+async function createOrIgnore (table, keys, reading) {
+  const object = {}
+  for (key of keys) {
+    object[key] = reading[key] !== undefined ? reading[key] : null
+  }
+  try {
+    await db(table).insert(object)
+  } catch (ex) {
+    if (ex.code == 'SQLITE_CONSTRAINT') {
+      return
+    }
+    throw ex
+  }
+}
+
+async function getForMeter (table, keys, meter) {
+  const readings = await db.from(table).where({ meterId: meter.id }).select(...keys)
+  return readings
+}
+
 module.exports = {
   createElectricity: async function (reading) {
-    const object = {}
-    for (key of ['meterId', 'timestamp', 'totalConsumptionKwhLow', 'totalConsumptionKwhHigh', 'totalProductionKwhLow', 'totalProductionKwhHigh', 'currentConsumptionKw', 'currentProductionKw']) {
-      object[key] = reading[key] || null
-    }
-    await db('electricityReadings').insert(object)
+    await createOrIgnore('electricityReadings', ELECTRICITY_KEYS, reading)
   },
 
   createGas: async function (reading) {
-    const object = {}
-    for (key of ['meterId', 'timestamp', 'totalConsumptionM3']) {
-      object[key] = reading[key] || null
-    }
-    await db('gasReadings').insert(object)
+    await createOrIgnore('gasReadings', GAS_KEYS, reading)
   },
 
   getElectricityForMeter: async function (meter) {
-    const readings = await db.from('electricityReadings').where({ meterId: meter.id }).select('meterId', 'timestamp', 'totalConsumptionKwhLow', 'totalConsumptionKwhHigh', 'totalProductionKwhLow', 'totalProductionKwhHigh', 'currentConsumptionKw', 'currentProductionKw')
-    return readings
+    return getForMeter('electricityReadings', ELECTRICITY_KEYS, meter)
   },
 
   getGasForMeter: async function (meter) {
-    const readings = await db.from('gasReadings').where({ meterId: meter.id }).select('meterId', 'timestamp', 'totalConsumptionM3')
-    return readings
+    return getForMeter('gasReadings', GAS_KEYS, meter)
   }
 }
--- server/services/readings.test.js
+++ server/services/readings.test.js
@@ -2,35 +2,39 @@
 
 const { expect } = require('../core/chai')
 
-const settings = require('./settings')
+const readings = require('./readings')
 const testDb = require('../core/testDb')
 
 describe('services/readings', () => {
   beforeEach(testDb.reset)
 
   describe('createElectricity', () => {
-    xit('creates a reading', () => {
+    it('creates a reading', async () => {
+      const reading = testDb.data.electricityReading
+      await readings.createElectricity(reading)
+      await expect(readings.getElectricityForMeter({ id: reading.meterId })).to.eventually.deep.equal([reading])
     })
     
-    xit('does not create a reading if a matching one already exists', () => {
+    it('does not create a reading if a matching one already exists', async () => {
+      const reading = testDb.data.electricityReading
+      await readings.createElectricity(reading)
+      await readings.createElectricity(reading)
+      await expect(readings.getElectricityForMeter({ id: reading.meterId })).to.eventually.deep.equal([reading])
     })
   })
 
   describe('createGas', () => {
-    xit('creates a reading', () => {
+    it('creates a reading', async () => {
+      const reading = testDb.data.gasReading
+      await readings.createGas(reading)
+      await expect(readings.getGasForMeter({ id: reading.meterId })).to.eventually.deep.equal([reading])
     })
     
-    xit('does not create a reading if a matching one already exists', () => {
-    })
-  })
-
-  describe('getElectricityForMeter', () => {
-    xit('returns all readings for the meter', () => {
-    })
-  })
-
-  describe('getGasForMeter', () => {
-    xit('returns all readings for the meter', () => {
+    it('does not create a reading if a matching one already exists', async () => {
+      const reading = testDb.data.gasReading
+      await readings.createGas(reading)
+      await readings.createGas(reading)
+      await expect(readings.getGasForMeter({ id: reading.meterId })).to.eventually.deep.equal([reading])
     })
   })
 })
